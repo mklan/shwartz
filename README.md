4f8a7519381f23341a4972ba4c2164aa91103629
Matthias Klan <matthias.klan@gmail.com>
2024-04-20 23:46:47 +0200

    initial

---
+++ README.md
@@ -0,0 +1,40 @@
+# shwartz
+
+An overlay to black out pixels on an OLED display. The goal is to reduce energy consumption. Can be of course also used on other panels, but with less effect.
+
+It is cross-platform but only tested on wayland.
+
+![Preview](https://github.com/mklan/shwartz/blob/main/preview.png?raw=true)
+
+## Build
+
+`cargo build --release`
+
+## Usage
+
+```bash
+Usage: shwartz [OPTIONS]
+
+Options:
+  -w, --width <WIDTH>    Width of the screen [default: 1920]
+  -h, --height <HEIGHT>  Height of the screen [default: 1080]
+  -e, --every <EVERY>    How often a pixel should be blacked out [default: 2]
+  -c, --color <COLOR>    Overwrite color [default: 0xFF000000]
+```
+
+Furthermore you need to tell your window manager how to handle shwartz windows. This means going fullscreen, on top, pinned on every desktop, unfocused to pass input and on which monitor.
+
+
+### Hyprland example
+
+Full overlay
+
+`hyprctl dispatch exec "[float;pin;nofocus;noblur;noborder;nodim;noanim;fakefullscreen]" /path/to/bin/shwartz`
+
+Kill it on demand with
+
+`pkill -f shwartz`
+
+Cover only a part (for example a bar or dock)
+
+`hyprctl dispatch exec "[float;pin;nofocus;noblur;noborder;nodim;noanim;fakefullscreen;move 0 0]" "/path/to/bin/shwartz --height 30 --every 3"`
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use minifb::{Key, Window, WindowOptions};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Width of the screen
+    #[arg(short, long, default_value_t = 1920)]
+    width: usize,
+
+    /// Height of the screen
+    #[arg(short, long, default_value_t = 1080)]
+    height: usize,
+
+    /// How often a pixel should be blacked out
+    #[arg(short, long, default_value_t = 2)]
+    every: usize,
+
+    /// Overwrite color
+    #[arg(short, long, default_value_t = 0xFF000000)]
+    color: u32,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let mut buffer: Vec<u32> = vec![0; args.width * args.height];
+
+    let mut window = Window::new(
+        "shwartz",
+        args.width,
+        args.height,
+        WindowOptions {
+            resize: false,
+            topmost: true,
+            borderless: true,
+            transparency: true,
+            ..WindowOptions::default()
+        },
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max 1 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_millis(1000)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for y in 0..args.height {
+            for x in 0..args.width {
+                let index = y * args.width + x;
+                if (x + y) % args.every == 0 {
+                    buffer[index] = args.color;
+                } else {
+                    buffer[index] = 0x00000000;
+                }
+            }
+        }
+
+        window
+            .update_with_buffer(&buffer, args.width, args.height)
+            .unwrap();
+    }
+}
